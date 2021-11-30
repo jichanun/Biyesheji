e27146a516493f303d3381f3d96069b5dc9231ac
ubuntu <261399820@qq.com>
2021-11-30 19:40:19 +0800

    加入期望画图

--- src/learn/scripts/CV_sub.py
+++ src/learn/scripts/CV_sub.py
@@ -36,9 +36,16 @@ def Position_To_Image(img):
         cv2.putText (img,( " ID:%d(%.2f,%.2f)"%(i,Act.x[i],Act.y[i])) ,(int(Act.x[i])*100-30,(1000-int(Act.y[i]*100))+50),font,0.7,(255,255,255),2)
         cv2.circle(img,(int (Act.x[i]*100),(1000-int(Act.y[i]*100))),20,(0,255,0),-1)
 
+def Expect_To_Image(img):
+    for i in range (2,6):
+        #cv2.putText (img,( " ID:%d(%.2f,%.2f)"%(i,Expect.x[i],Expect.y[i])) ,(int(Expect.x[i]*100)-30,(1000-int(Expect.y[i]*100))+50),font,0.7,(255,255,255),2)
+        cv2.circle(img,(int (Expect.x[i]*100),(1000-int(Expect.y[i]*100))),10,(0,0,255),-1)
+        cv2.line(img,(int (Act.x[i]*100),(1000-int(Act.y[i]*100))),(int (Expect.x[i]*100),(1000-int(Expect.y[i]*100))),(0,0,255),2)
+
 def ExpectInfoCallback(msg):
-    #rospy.loginfo("CV received Expected position x:%f y:%f", msg.x[0],msg.y[0])
-    pass
+    rospy.loginfo("CV received Expected position x:%f y:%f", msg.x[0],msg.y[0])
+    global Expect
+    Expect = msg
 
 def person_subscriber():
 	# ROS节点初始化
@@ -53,8 +60,10 @@ def person_subscriber():
 
     while not rospy.is_shutdown():
         global Act
+        global Expect
         img2 =  np.zeros((1024,1024,3),np.uint8)
         Position_To_Image(img2)
+        Expect_To_Image(img2)
         plt.imshow(img2)
         plt.show(block = False)
         plt.title("All The Robot's Position \n\n",font1) 
