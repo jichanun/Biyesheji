e3ac791238d424aaeacd026db24d645870f3cbfa
ubuntu <261399820@qq.com>
2022-03-30 13:00:37 +0800

    历史路径

--- src/learn/scripts/CV_sub.py
+++ src/learn/scripts/CV_sub.py
@@ -25,12 +25,15 @@ Expect = expectp()
 
 Plotx=[]
 Ploty=[]
+PlotAct=[]
+PlotExp=[]
 
 plt.show()            
 def ActualInfoCallback(msg):
     #rospy.loginfo("CV received Actual position: X:%f  Y:%f ", msg.x[0], msg.y[0])
     global Act
     Act = msg
+    PlotAct.append(Act)
 
 def Position_To_Image(img):
     cv2.putText (img,( " Target:(%.2f,%.2f)"%(Act.x[0],Act.y[0])) ,(int(Act.x[0])*100-30,(1000-int(Act.y[0]*100))-50),font,0.7,(255,255,255),2)
@@ -44,6 +47,13 @@ def Expect_To_Image(img):
         #cv2.putText (img,( " ID:%d(%.2f,%.2f)"%(i,Expect.x[i],Expect.y[i])) ,(int(Expect.x[i]*100)-30,(1000-int(Expect.y[i]*100))+50),font,0.7,(255,255,255),2)
         cv2.circle(img,(int (Expect.x[i]*100),(1000-int(Expect.y[i]*100))),10,(0,0,255),-1)
         cv2.line(img,(int (Act.x[i]*100),(1000-int(Act.y[i]*100))),(int (Expect.x[i]*100),(1000-int(Expect.y[i]*100))),(0,0,255),2)
+
+def Path_plot(img):
+    for i in range (len(PlotAct)-1) :
+        cv2.line(img,(int (PlotAct[i+1].x[2]*100),(1000-int(PlotAct[i+1].y[2]*100))),(int (PlotAct[i+1].x[2]*100),(1000-int(PlotAct[i+1].y[2]*100))),(0,255,255),5)
+        cv2.line(img,(int (PlotAct[i+1].x[3]*100),(1000-int(PlotAct[i+1].y[3]*100))),(int (PlotAct[i+1].x[3]*100),(1000-int(PlotAct[i+1].y[3]*100))),(255,255,0),5)
+        cv2.line(img,(int (PlotAct[i+1].x[4]*100),(1000-int(PlotAct[i+1].y[4]*100))),(int (PlotAct[i+1].x[4]*100),(1000-int(PlotAct[i+1].y[4]*100))),(255,0,255),5)
+        cv2.line(img,(int (PlotAct[i+1].x[5]*100),(1000-int(PlotAct[i+1].y[5]*100))),(int (PlotAct[i+1].x[5]*100),(1000-int(PlotAct[i+1].y[5]*100))),(255,255,255),5)
 
 def ExpectInfoCallback(msg):
     rospy.loginfo("CV received Expected position x:%f y:%f", msg.x[0],msg.y[0])
@@ -62,10 +72,12 @@ def ExpectInfoCallback(msg):
         if temp > maxy :
             maxy=temp
     
-    print ("aver = = = = = = == =%.2f \n\n\n\n\n\n\n\n\n\n\n\n",aver)
+    print ("aver = = = = = = == =%.2f \n\n\n\n\n\n\n\n\n\n\n",aver)
+    print(len(PlotExp))
+
     Plotx.append(aver)
     Ploty.append ( maxy)
-
+    PlotExp.append(Expect)
     
 def person_subscriber():
 	# ROS节点初始化
@@ -81,10 +93,10 @@ def person_subscriber():
     while not rospy.is_shutdown():
         global Act
         global Expect
-        '''
         img2 =  np.zeros((1024,1024,3),np.uint8)
         Position_To_Image(img2)
         Expect_To_Image(img2)
+        Path_plot(img2)
         plt.imshow(img2)
         plt.show(block = False)
         plt.title("All The Robot's Position \n\n",font1) 
@@ -95,20 +107,19 @@ def person_subscriber():
         plt.yticks([1000,800,600,400,200,0],[0,2,4,6,8,10])
         plt.xticks([0,200,400,600,800,1000],[0,2,4,6,8,10])
         #plt.gca().invert_yaxis()
+        plt.pause(0.001)
+        plt.clf()
+    rate.sleep()
+    rospy.spin()
 '''
         plt.title("matplot")
         plt.xlabel("x")
         plt.ylabel("y")
         plt.plot(Plotx,Ploty)
         plt.show()
+        '''
 
 
-        #plt.pause(0.001)
-        #plt.clf()
-
-
-    rate.sleep()
-    #rospy.spin()
 
 if __name__ == '__main__':
     person_subscriber()
